afda1b213ef30503548e2e159834e302b729229b
IvánBorrazGarcía <ivanborrazgarcia@gmail.com>
2023-10-04 13:57:11 +0200

    comida

--- primeraprueba.java
+++ primeraprueba.java
@@ -4,7 +4,7 @@ public class primeraprueba {
 
 		System.out.println("Borraz García");
 		System.out.println("Tengo 17 años y nací el 14 de diciembre del 2005");
-		
+		System.out.println("Mi comida favorita es el pulpo");	
 	}
 
 }
