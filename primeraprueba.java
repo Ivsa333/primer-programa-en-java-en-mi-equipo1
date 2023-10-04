c7428a8a3dc5abf9b54d74b915f6ab12fa2ecda4
IvánBorrazGarcía <ivanborrazgarcia@gmail.com>
2023-10-04 13:41:26 +0200

    edad

--- primeraprueba.java
+++ primeraprueba.java
@@ -3,7 +3,8 @@ public class primeraprueba {
 	public static void main(String args[]) {
 
 		System.out.println("Borraz García");
-
+		System.out.println("Tengo 17 años y nací el 14 de diciembre del 2005");
+		
 	}
 
 }
