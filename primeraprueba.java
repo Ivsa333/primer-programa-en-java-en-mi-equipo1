8e94d086b5339d93e077d45da05d1069fc80534f
IvánBorrazGarcía <ivanborrazgarcia@gmail.com>
2023-10-10 13:11:16 +0200

    música

--- primeraprueba.java
+++ primeraprueba.java
@@ -2,9 +2,9 @@ public class primeraprueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Borraz García");
 		System.out.println("Tengo 17 años y nací el 14 de diciembre del 2005");
-		System.out.println("Mi comida favorita es el pulpo");	
+		System.out.println("Mi comida favorita es el pulpo");
+		System.out.println("No tengo grupo de música o cantante favorito");	
 	}
 
 }
